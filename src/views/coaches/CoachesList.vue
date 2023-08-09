296f03c6037e1ccbd8460ee041d3f3518151810e
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-08-10 01:59:58 +0400

    feat: add pages

---
+++ src/views/coaches/CoachesList.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>Filter</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    List of coaches
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
