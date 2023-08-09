599e06e255825092d2bc490681049cf1f7b7ead6
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-08-09 23:08:39 +0400

    feat: add routes settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,44 @@ import HomeView from "../views/HomeView.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
+    redirect: "/coaches",
+  },
+  {
+    path: "/coaches",
+    name: "coaches",
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/coaches/:id",
+    name: "coachId",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    children: [{ path: "contact", component: HomeView }], // coaches/id/contact
+  },
+  {
+    path: "/register",
+    name: "register",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/requests",
+    name: "requests",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/:notFound(.*)",
+    name: "not-found",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
