310e1f4bbaca5cf96693c4a1f08d595e3cee5af5
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-08-10 17:10:41 +0400

    fix: change structure of routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,52 +1,42 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import CoachesList from '@/views/coaches/CoachesList.vue';
+import CoachDetail from '@/views/coaches/CoachDetail.vue';
+import CoachRegistration from '@/views/coaches/CoachRegistration.vue';
+import ContactCoach from '@/views/requests/ContactCoach.vue';
+import RequestsRecieved from '@/views/requests/RequestsRecieved.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    redirect: "/coaches",
+    path: '/',
+    redirect: '/coaches',
   },
   {
-    path: "/coaches",
-    name: "coaches",
-    component: HomeView,
+    path: '/coaches',
+    name: 'coaches',
+    component: CoachesList,
   },
   {
-    path: "/coaches/:id",
-    name: "coachId",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-    children: [{ path: "contact", component: HomeView }], // coaches/id/contact
+    path: '/coaches/:id',
+    name: 'coachId',
+    props: true,
+    component: CoachDetail,
+    children: [{ path: 'contact', component: ContactCoach }],
   },
   {
-    path: "/register",
-    name: "register",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/register',
+    name: 'register',
+    component: CoachRegistration,
   },
   {
-    path: "/requests",
-    name: "requests",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/requests',
+    name: 'requests',
+    component: RequestsRecieved,
   },
   {
-    path: "/:notFound(.*)",
-    name: "not-found",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/:notFound(.*)',
+    name: 'not-found',
+    component: NotFound,
   },
 ];
 
