66e732032b097335b7b9cf88c11eff35f38bb505
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-08-11 16:44:47 +0400

    feat: implement outputting requests

--- src/store/modules/requests/getters.ts
+++ src/store/modules/requests/getters.ts
@@ -1,1 +1,14 @@
-export default {};
+import { GetterTree } from 'vuex';
+import { RequestsState } from '@/types/RequestsState';
+import { RootState } from '@/types/RootState';
+
+const getters: GetterTree<RequestsState, RootState> = {
+  requests(state) {
+    return state.requests;
+  },
+  hasRequests(state) {
+    return state.requests && state.requests.length > 0;
+  },
+};
+
+export default getters;
--- src/views/requests/RequestsRecieved.vue
+++ src/views/requests/RequestsRecieved.vue
@@ -4,11 +4,38 @@
       <header>
         <h2>Requests Recieved</h2>
       </header>
-      <ul></ul>
-      <h3>You haven't recieved any requests yet!</h3>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="req in recievedRequests"
+          :key="req.id"
+          :email="req.email"
+          :message="req.message"
+        ></request-item>
+      </ul>
+      <h3 v-else>You haven't recieved any requests yet!</h3>
     </base-card>
   </section>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ContactData } from '@/types/RequestsState';
+import RequestItem from '@/components/requests/RequestItem.vue';
+
+export default defineComponent({
+  components: {
+    RequestItem,
+  },
+  computed: {
+    recievedRequests(): ContactData[] {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+});
+</script>
 
 <style lang="scss" scoped>
 header {
